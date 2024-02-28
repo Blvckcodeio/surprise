68444daf21f578521f63ca09456df4474cb26e9a
Blvckcodeio <ninokayglobal@gmail.com>
2024-02-28 21:32:57 +0100

    yy

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./main.css";
-import Firstcard from "../../components/firstcard";
+import Firstcard from "../../components/Firstcard";
 
 export default function Home() {
 
